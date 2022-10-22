2534a8411241a2ba82d0d0190e297912081b2087
dayumsam <dayumsam22@gmail.com>
2022-10-22 17:41:26 -0400

    added comments and removed extra imports

--- image_upload.py
+++ image_upload.py
@@ -1,7 +1,4 @@
-import base64
 import os
-import sys
-from pathlib import Path
 
 from imagekitio import ImageKit
 
@@ -9,26 +6,34 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Imagekit config
 imagekit = ImageKit(
     public_key=os.getenv('IMAGE_KIT_PUBLIC_KEY'),
     private_key=os.getenv('IMAGE_KIT_PRIVATE_KEY'),
     url_endpoint=os.getenv('IMAGE_KIT_URL')
 )
 
+# Folder with images
+# Images should be in a folder with the name of the style
 baseFolder = 'styles/'
 
+# Loop through each folder
 with os.scandir(baseFolder) as entries:
     for entry in entries:
 
-        fileFolder = baseFolder + entry.name
+        fileFolder = baseFolder + entry.name  # generate folder names
 
+        # Loop through each file inside the folder
         with os.scandir(fileFolder) as files:
             for file in files:
                 if file.is_file():
-                    imgPath = fileFolder + "/" + file.name
+                    imgPath = fileFolder + "/" + file.name  # generate file names
 
+                    # generate a tag from the folder name
+                    # converting the name to lowercase and replacing spaces with an underscore
                     tag = entry.name.lower().replace(" ", "_")
 
+                    # uploading to imagekit
                     upload = imagekit.upload(
                         file=open(imgPath, "rb"),
                         file_name=tag,
@@ -38,6 +43,7 @@ with os.scandir(baseFolder) as entries:
                         },
                     )
 
+                    # print status of each upload
                     print("Upload binary", upload, end="\n\n")
 
-print("done")
+print("done")  # end program
